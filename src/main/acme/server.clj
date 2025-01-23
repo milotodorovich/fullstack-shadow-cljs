27e84cd9c9371fd16c4f1055675c1948a19754f7
Milo Todorovich <milo.todorovich@gmail.com>
2025-01-22 21:09:44 -0600

    Serve html; connect cljs app to console.

--- src/main/acme/server.clj
+++ src/main/acme/server.clj
@@ -1,6 +1,14 @@
-(ns acme.server)
+(ns acme.server
+  (:require
+   [ring.middleware.file :as ring-file]
+   [ring.middleware.file-info :as ring-file-info]))
 
-(defn handler [req]
+(defn my-handler [req]
   {:status 200
    :headers {"content-type" "text/plain"}
-   :body "Hello Shadow-World!"})
+   :body "Hello World!"})
+
+(def handler
+  (-> my-handler
+      (ring-file/wrap-file "public")
+      (ring-file-info/wrap-file-info)))
