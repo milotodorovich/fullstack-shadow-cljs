88a8106f2479f71f3137fa40862d43b8d8ba4764
Milo Todorovich <milo.todorovich@gmail.com>
2025-01-24 21:01:04 -0600

    Add reagent (#1)
    * Display FE using reagent.
    * Optimize js load in html.

--- src/main/acme/frontend/app.cljs
+++ src/main/acme/frontend/app.cljs
@@ -1,4 +1,25 @@
-(ns acme.frontend.app)
+(ns acme.frontend.app
+  (:require
+   [reagent.core :as r]
+   [reagent.dom.client :as rdom]))
 
-(defn init []
-  (println "Hello SPA-World"))
+(defn app []
+  [:h1 "Hello, Ted!"])
+
+(defn app-element []
+  (js/document.getElementById "root"))
+
+(defonce root (rdom/create-root (app-element)))
+
+(defn ^:export init
+  "Run application startup logic."
+  []
+  (.render root (r/as-element [app])))
+
+(defn ^:dev/after-load re-render
+  []
+  ;; The `:dev/after-load` metadata causes this function to be called
+  ;; after shadow-cljs hot-reloads code.
+  ;; This function is called implicitly by its annotation.
+  (init))
+
